efb7b29cb09eea90c83f96bb2ff50832e7cb9024
cjhaddad & Shannon Staszak & Logan Price & SerwahBonsu <pair+cjhaddad.fancyultrafresh.logandsprice.serwahbonsu@devbootcamp.com>
2015-05-14 17:16:22 -0500

    twilio api working! user method made to send texts

--- connect/app/controllers/index.rb
+++ connect/app/controllers/index.rb
@@ -1,4 +1,29 @@
+# require 'twilio-ruby'
+
 get '/' do
+
+  # accountSID = 'AC26b99524e4e895228567bb7802caf7a7'
+  # authToken = '2c9b943fe29fef0b293e34d05db5341c'
+
+  # p ENV['ACCOUNT_SID']
+  # p ENV['AUTH_TOKEN']
+
+  @client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
+
+  from = '+13176717463'
+
+  friends = ["+13179858281",
+             "+17083051838",
+             "+18473704680"]
+
+  friends.each do |number|
+    message = @client.account.messages.create(
+                                              from: from,
+                                              to: number,
+                                              body: "heeyeyeyeyeyeyeyeyey, did you get this?"
+                                              )
+  puts "sent message to #{number}"
+  end
 
   erb :index
 end
--- connect/app/models/user.rb
+++ connect/app/models/user.rb
@@ -3,4 +3,19 @@ class User < ActiveRecord::Base
   has_one :seek_connection, class_name: "Connection", foreign_key: :seeker_id
 
 
+  def self.text_helpers(url, recipients)
+    @client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
+
+    from = '+13176717463'
+
+    recipients.each do |number|
+      message = @client.account.messages.create(
+                                                from: from,
+                                                to: number,
+                                                body: "Someone would like to connect. Follow this link: #{url}"
+                                                )
+    puts "sent message to #{number}"
+    end
+  end
+
 end
