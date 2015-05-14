cf59a6c35e96f5dfee0699cc60f416afc8d577ee
cjhaddad & Shannon Staszak & Logan Price & SerwahBonsu <pair+cjhaddad.fancyultrafresh.logandsprice.serwahbonsu@devbootcamp.com>
2015-05-14 16:09:49 -0500

    added helper methods

---
+++ connect/app/helpers/helpers.rb
@@ -0,0 +1,22 @@
+helpers do
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def current_user_id
+    session[:user_id]
+  end
+
+  def logged_in?
+    session[:user_id] != nil
+  end
+
+  def login_user
+    session[:user_id] = current_user.id
+  end
+
+  def logout_user
+    session[:user_id] = nil
+  end
+end
