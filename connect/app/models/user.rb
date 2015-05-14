5ea30ae15d0c10df9b362fbda05e3802c2c44c03
Shannon Staszak & SerwahBonsu <pair+fancyultrafresh.serwahbonsu@devbootcamp.com>
2015-05-14 16:06:46 -0500

    add user-auths

--- connect/app/models/user.rb
+++ connect/app/models/user.rb
@@ -2,5 +2,20 @@ class User < ActiveRecord::Base
   has_many :help_connections, class_name: "Connection", foreign_key: :helper_id
   has_one :seek_connection, class_name: "Connection", foreign_key: :seeker_id
 
+  include BCrypt
+
+  def self.authenticate(username, password)
+    user = User.find_by_username(username)
+    return user if user && (user.password == password)
+    nil
+  end
+
+  def password
+    @password || = Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    self.hashed_password = Password.create(new_password)
+  end
 
 end
